dc1162bdbab668b0ee9107e89e2a3a81ce1ec4b0
Eithan Mendez <eithan.mndez@outlook.com>
2023-10-14 17:22:04 -0600

    Added comment for dsicrminator class

--- swagger.polymorphism.core/Discriminators/DeviceDiscriminator.cs
+++ swagger.polymorphism.core/Discriminators/DeviceDiscriminator.cs
@@ -3,6 +3,10 @@
 namespace swagger.polymorphism.core.Discriminators;
 public static class DeviceDiscriminator
 {
+    /*
+     * 'DiscriminatorKey' is the attribute that is going to be isnerted when Swagger initialize
+     * to determine what type of schema is being passed
+     * **/
     public const string DiscriminatorKey = "DeviceType";
 
     public const string PhoneDevice = nameof(ElectronicDeviceType.Smarthphone);
